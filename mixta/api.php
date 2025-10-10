1e953156998a3199a049987e2871d5c19b8d36e1
Eva.Rodriguez <evarodriguez.24@campuscamara.es>
2025-10-10 08:43:28 +0200

    ejercicio foto

--- mixta/api.php
+++ mixta/api.php
@@ -13,11 +13,12 @@ function getData() {
         {"slug": "electrohogar", "nombre": "Electrohogar"}
       ],
       "productos": [
-        {"id": 1, "nombre": "Mueble Cocina Blanco", "precio": 799.99, "categoria_slug": "cocinas", "descripcion": "Cocina modular con acabado blanco mate."},
-        {"id": 2, "nombre": "Encimera Granito", "precio": 299.00, "categoria_slug": "cocinas", "descripcion": "Encimera resistente de granito natural."},
-        {"id": 3, "nombre": "Lavabo Suspendido", "precio": 159.50, "categoria_slug": "banos", "descripcion": "Lavabo con instalación suspendida y sifón oculto."},
-        {"id": 4, "nombre": "Mampara Ducha 120", "precio": 220.00, "categoria_slug": "banos", "descripcion": "Mampara corredera templada 6mm."},
-        {"id": 5, "nombre": "Lavadora 8kg A+++", "precio": 399.90, "categoria_slug": "electrohogar", "descripcion": "Lavadora eficiente con 15 programas."}
+       {"id": 1, "nombre": "Mueble Cocina Blanco", "precio": 799.99, "categoria_slug": "cocinas", "descripcion": "Cocina modular con acabado blanco mate.", "img": "https://www.murellicucine.com/wp-content/uploads/2019/10/Captura-de-pantalla-2019-10-10-a-las-13.37.39.png"},
+        {"id": 2, "nombre": "Encimera Granito", "precio": 299.00, "categoria_slug": "cocinas", "descripcion": "Encimera resistente de granito natural.", "img": "https://storage.googleapis.com/catalog-pictures-carrefour-es/catalog/pictures/hd_510x_/8059019005195_1.jpg"},
+        {"id": 3, "nombre": "Lavabo Suspendido", "precio": 159.50, "categoria_slug": "banos", "descripcion": "Lavabo con instalación suspendida y sifón oculto.", "img": "https://www.murellicucine.com/wp-content/uploads/2019/10/Captura-de-pantalla-2019-10-10-a-las-13.37.39.png"},
+        {"id": 4, "nombre": "Mampara Ducha 120", "precio": 220.00, "categoria_slug": "banos", "descripcion": "Mampara corredera templada 6mm.", "img" : "https://www.murellicucine.com/wp-content/uploads/2019/10/Captura-de-pantalla-2019-10-10-a-las-13.37.39.png"},
+        {"id": 5, "nombre": "Lavadora 8kg A+++", "precio": 399.90, "categoria_slug": "electrohogar", "descripcion": "Lavadora eficiente con 15 programas.", "img": "https://www.murellicucine.com/wp-content/uploads/2019/10/Captura-de-pantalla-2019-10-10-a-las-13.37.39.png"}
+ 
       ]
     }';
     return json_decode($json, true);
